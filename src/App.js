a475c728b0fc6ec7639eb1a213cd7c2d44030d45
Yosmar Hinestroza <consultor_yhinestroza@vitamina.cl>
2022-03-29 17:49:12 -0300

    Feat: se crea realiza la condicion de ocultar campos en la table principal cuando se detecta que es mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 /** @format */
 import React, { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
 import "./App.css";
 import ModalDetalle from "./components/ModalDetalle";
 import moment from "moment";
-
+import { Spinner } from "react-bootstrap";
 
 function App() {
   const [dataCoins, setDataCoins] = useState(null);
@@ -11,8 +12,8 @@ function App() {
   const [dataModal, setDataModal] = useState(null);
   const [stateViews, setStateViews] = useState(false);
   const [txtForm, setTextForm] = useState({
-    search: ''
-  })
+    search: "",
+  });
   const { search } = txtForm;
 
   useEffect(() => {
@@ -34,7 +35,6 @@ function App() {
       .catch((err) => {
         console.log(err.message);
       });
-
   };
 
   const handleChange = (e) => {
@@ -42,16 +42,16 @@ function App() {
   };
 
   const getSearchcoins = (search) => {
-    const data = dataCoins.filter(coins => coins.name.toLowerCase().includes(search.toLowerCase()));
+    const data = dataCoins.filter((coins) =>
+      coins.name.toLowerCase().includes(search.toLowerCase())
+    );
     if (data.length > 0) {
-      setDataCoins(data)
+      setDataCoins(data);
     } else {
-      alert('Sin resultado con los parametros...')
+      alert("Sin resultado con los parametros...");
       getDatosApiMarket();
     }
   };
-
-
 
   const handleOpenModal = () => {
     setOpenModal(!OpenModal);
@@ -70,79 +70,99 @@ function App() {
         style={{ maxHeight: "600px" }}
       >
         <table
-          className="table text-white"
+          className="responsive table text-white"
           style={{ minHeight: "400px", backgroundColor: "#111b21" }}
         >
           <thead>
             <tr>
-              <th></th>
+              {!isMobile ? <th></th> : ""}
               <th className="text-start fw-bold p-4">COINS</th>
               <th className="text-end fw-bold p-4">CURRENT PRICE</th>
-              <th className="text-end fw-bold p-4">HIGH 24h</th>
-              <th className="text-end fw-bold p-4">LOW 24h</th>
-              <th className="text-end fw-bold p-4">VOLUMEN 24h</th>
-              <th className="text-end fw-bold p-4">
-                OUTSTANDING SHARED PRICES
-              </th>
+              {!isMobile ? (
+                <>
+                  <th className="text-end fw-bold p-4">HIGH 24h</th>
+                  <th className="text-end fw-bold p-4">LOW 24h</th>
+                  <th className="text-end fw-bold p-4">VOLUMEN 24h</th>
+                  <th className="text-end fw-bold p-4">
+                    OUTSTANDING SHARED PRICES
+                  </th>
+                </>
+              ) : (
+                " "
+              )}
             </tr>
           </thead>
           <tbody>
             {dataCoins && dataCoins.length > 0
               ? dataCoins.map((d, index) => (
-                <tr className="color-celda"
-                  key={d.id}
-                  onClick={() => handleDetalle(d)}>
-                  <td className="fw-bold text-center text-uppercase p-4">
-                    {index + 1}
-                  </td>
-                  <td>
-                    <img src={d.image} alt={d.name} className="m-2" />{" "}
-                    {d.name} <b className="text-uppercase">{d.symbol}</b> {d.id}
-                  </td>
-                  <td className="text-end fw-bold p-4">
-                    {d.current_price.toLocaleString()}
-                  </td>
-                  <td className="text-end fw-bold text-success p-4">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-up-short"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"
-                      />
-                    </svg>{" "}
-                    {d.high_24h.toLocaleString()}
-                  </td>
-                  <td className="text-end fw-bold text-danger p-4">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-down-short"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"
-                      />
-                    </svg>
-                    {d.low_24h.toLocaleString()}
-                  </td>
-                  <td className="text-end fw-bold p-4">
-                    $ {d.total_volume.toLocaleString("es")}{" "}
-                  </td>
-                  <td className="text-end fw-bold p-4">
-                    {d.circulating_supply.toLocaleString("es")}{" "}
-                    <b className="text-uppercase">{d.symbol}</b>
-                  </td>
-                </tr>
-              ))
+                  <tr
+                    className="color-celda"
+                    key={d.id}
+                    onClick={() => handleDetalle(d)}
+                  >
+                    {!isMobile ? (
+                      <>
+                        <td className="fw-bold text-center text-uppercase p-4">
+                          {index + 1}
+                        </td>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                    <td>
+                      <img src={d.image} alt={d.name} className="m-2" />{" "}
+                      {d.name} <b className="text-uppercase">{d.symbol}</b>{" "}
+                    </td>
+                    <td className="text-end fw-bold p-4">
+                      {d.current_price.toLocaleString()}
+                    </td>
+                    {!isMobile ? (
+                      <>
+                        <td className="text-end fw-bold text-success p-4">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-arrow-up-short"
+                            viewBox="0 0 16 16"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"
+                            />
+                          </svg>{" "}
+                          {d.high_24h.toLocaleString()}
+                        </td>
+                        <td className="text-end fw-bold text-danger p-4">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-arrow-down-short"
+                            viewBox="0 0 16 16"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"
+                            />
+                          </svg>
+                          {d.low_24h.toLocaleString()}
+                        </td>
+                        <td className="text-end fw-bold p-4">
+                          $ {d.total_volume.toLocaleString("es")}{" "}
+                        </td>
+                        <td className="text-end fw-bold p-4">
+                          {d.circulating_supply.toLocaleString("es")}{" "}
+                          <b className="text-uppercase">{d.symbol}</b>
+                        </td>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </tr>
+                ))
               : ""}
           </tbody>
         </table>
@@ -170,13 +190,18 @@ function App() {
                 ? moment(dataCoins?.last_updated).format("DD-MM-YYYY HH:mm:ss")
                 : ""}
             </b>
-            <a href="https://www.coingecko.com/" target="_blank" rel="noreferrer">coingecko.com</a>
+            <a
+              href="https://www.coingecko.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              coingecko.com
+            </a>
             <div className="clearfix my-3 m-auto">
-
               <h1 className="title-app mb-2">
-                AppCoins - NFT</h1>
+                Cryptocurrency Prices by Market Cap App
+              </h1>
             </div>
-
 
             <div className="clearfix my-3 m-auto">
               <input
@@ -188,20 +213,32 @@ function App() {
                 onChange={handleChange}
                 onBlur={() => getSearchcoins(search)}
               />
-              <button className="btn btn-dark float-end my-3"
-                onClick={getDatosApiMarket}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                  <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
+              <button
+                className="btn btn-dark float-end my-3"
+                onClick={getDatosApiMarket}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="currentColor"
+                  className="bi bi-arrow-clockwise"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
+                  />
                   <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
                 </svg>
-
               </button>
             </div>
 
             <div className="clearfix my-4">
-
-              <button className="btn btn-dark m-1"
-                onClick={() => setStateViews(false)}>
+              <button
+                className="btn btn-dark m-1"
+                onClick={() => setStateViews(false)}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="25"
@@ -214,8 +251,10 @@ function App() {
                   <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
                 </svg>
               </button>
-              <button className="btn btn-dark m-1"
-                onClick={() => setStateViews(true)}>
+              <button
+                className="btn btn-dark m-1"
+                onClick={() => setStateViews(true)}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="25"
@@ -231,7 +270,9 @@ function App() {
           </div>
           {/*  <TredingCoins /> */}
           <hr />
-          {!stateViews ? tableDatos :
+          {!stateViews ? (
+            tableDatos
+          ) : (
             <div
               className="row"
               style={{
@@ -252,16 +293,17 @@ function App() {
                         className="img-fluid"
                         src={`${data.image}`}
                         alt={data.name}
-                        width="100"
+                        width="120"
                       />
                       <div className="my-2">
                         <b className="text-center text-uppercase fw-bold">
                           {data.name}
                           <div className="my-3 text-center">
-                            <span className="text-center">PRICE: ${data.current_price.toLocaleString()}</span>
+                            <span className="text-center">
+                              PRICE: ${data.current_price.toLocaleString()}
+                            </span>
                             {/*  <b className="float-end text-success">$ {data.high_24h.toLocaleString()}</b> */}
                           </div>
-
                         </b>
                       </div>
                     </div>
@@ -269,13 +311,15 @@ function App() {
                 </div>
               ))}
             </div>
-          }
+          )}
         </>
       ) : (
-        <h4>Loading...</h4>
-      )
-      }
-    </div >
+        <div className="loading-container text-center">
+          <Spinner animation="grow" variant="light" />
+          <h4>Loading informations...</h4>
+        </div>
+      )}
+    </div>
   );
 }
 
